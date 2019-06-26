2deba9dd83be6268327631471b9c6f4eb67d5466
Varvara Babkova <varvara.munday@gmail.com>
2019-06-26 19:42:03 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,9 +2,9 @@
 class Person
 
   attr_reader :name
-  attr_accessor :bank_account
+  attr_accessor :bank_account, :happiness
 
-  def initialize (name, bank_account = 25)
+  def initialize (name, bank_account = 25, happiness = 8)
     @name = name
     @bank_account = bank_account
   end
